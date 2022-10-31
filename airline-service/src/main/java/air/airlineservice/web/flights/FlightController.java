a2ffabae4ca2bb62401ef812525e4dc112c81c6d
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-10-31 22:02:20 +0100

    Airline service: created flight controller

---
+++ airline-service/src/main/java/air/airlineservice/web/flights/FlightController.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2002-2021 Solera.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package air.airlineservice.web.flights;
+
+import air.airlineservice.service.flight.Flight;
+import air.airlineservice.service.flight.FlightService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(path = "/flights", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class FlightController {
+    private final FlightService flightService;
+
+    @Autowired
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    @GetMapping
+    public List<Flight> getAll() {
+        return flightService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Flight getById(@PathVariable Long id) {
+        return flightService.findById(id)
+                .orElseThrow(() -> new  NoSuchElementException("No flight with ID " + id));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("@flightAccessHandler.canPost(#flight.airline.id)")
+    public Flight post(@RequestBody @Valid Flight flight) {
+        return flightService.save(flight);
+    }
+
+    @PatchMapping(value = "/{id}")
+    @PreAuthorize("@flightAccessHandler.canPatch(#flight.airline.id)")
+    public Flight patchById(@PathVariable Long id,
+                             @RequestBody Flight flight) {
+        flight.setId(id);
+        return flightService.update(flight);
+    }
+}
---
+++ airline-service/src/test/java/air/airlineservice/web/flights/FlightControllerTest.java
@@ -0,0 +1,201 @@
+package air.airlineservice.web.flights;
+
+import air.airlineservice.service.flight.Flight;
+import air.airlineservice.service.flight.FlightService;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Tag("category.IntegrationTest")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FlightControllerTest {
+    private static String newFlight1Json;
+    private static String newFlight2Json;
+    private static String updatedFlight1Json;
+    private static String updatedFlight2Json;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private FlightService flightService;
+
+    @BeforeAll
+    public static void createNewFlightJson() {
+        newFlight1Json = "{" +
+                "\"airline\": {" +
+                    "\"id\":\"1\"" +
+                "}," +
+                "\"from\": {" +
+                    "\"country\": \"country\", " +
+                    "\"airport\": \"airport\", " +
+                    "\"gate\": \"1\"" +
+                "}, " +
+                "\"to\": {" +
+                    "\"country\": \"country\", " +
+                    "\"airport\": \"airport\", " +
+                    "\"gate\": \"1\"" +
+                "}, " +
+                "\"dateTime\": \"2022-10-31T21:51:36.867100600\"" +
+                "}";
+
+        newFlight2Json = "{" +
+                "\"airline\": {" +
+                    "\"id\":\"2\"" +
+                "}," +
+                "\"from\": {" +
+                    "\"country\": \"country\", " +
+                    "\"airport\": \"airport\", " +
+                    "\"gate\": \"1\"" +
+                "}, " +
+                "\"to\": {" +
+                    "\"country\": \"country\", " +
+                    "\"airport\": \"airport\", " +
+                    "\"gate\": \"1\"" +
+                "}, " +
+                "\"dateTime\": \"2022-10-31T21:51:36.867100600\"" +
+                "}";
+    }
+
+    @BeforeAll
+    private static void createUpdatedFlightJsons() {
+        updatedFlight1Json = "{" +
+                "\"airline\": {" +
+                    "\"id\":\"2\"" +
+                "}," +
+                "\"from\": {" +
+                    "\"country\": \"updated-country\", " +
+                    "\"airport\": \"updated-airport\", " +
+                    "\"gate\": \"2\"" +
+                "}, " +
+                "\"to\": {" +
+                    "\"country\": \"updated-country\", " +
+                    "\"airport\": \"updated-airport\", " +
+                    "\"gate\": \"3\"" +
+                "}, " +
+                "\"dateTime\": \"2022-10-31T21:51:36.867100600\"" +
+                "}";
+
+        updatedFlight2Json = "{" +
+                "\"airline\": {" +
+                    "\"id\":\"3\"" +
+                "}," +
+                "\"from\": {" +
+                    "\"country\": \"updated-country2\", " +
+                    "\"airport\": \"updated-airport2\", " +
+                    "\"gate\": \"7\"" +
+                "}, " +
+                "\"to\": {" +
+                    "\"country\": \"updated-country2\", " +
+                    "\"airport\": \"updated-airport2\", " +
+                    "\"gate\": \"10\"" +
+                "}, " +
+                "\"dateTime\": \"2022-10-31T21:51:36.867100600\"" +
+                "}";
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void shouldReturnFlightsOnFlightsGetRequest() throws Exception {
+        mvc.perform(get("/flights"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void shouldReturnFlightByIdOnFlightGetRequest() throws Exception {
+        mvc.perform(get("/flights/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    public void shouldReturnSavedFlightOnFlightsPostRequestWhenUserIsAdmin() throws Exception {
+        postAndExpect(newFlight1Json, status().isCreated());
+    }
+
+    public void postAndExpect(String data, ResultMatcher status) throws Exception {
+        mvc.perform(post("/flights")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "owner2", authorities = "USER")
+    public void shouldReturnSavedFlightOnFlightsPostRequestWhenUserIsResourceOwner() throws Exception {
+        postAndExpect(newFlight2Json, status().isCreated());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    public void shouldReturnUpdatedFlightOnFlightPatchRequestWhenUserIsAdmin() throws Exception {
+        Flight initial = flightService.findById(2L).orElseThrow();
+        patchByIdAndExpect(2L, updatedFlight1Json, status().isOk());
+
+        Flight updated = flightService.findById(2L).orElseThrow();
+        assertThat(updated, is(not(equalTo(initial))));
+    }
+
+    private void patchByIdAndExpect(Long id, String data, ResultMatcher status) throws Exception {
+        mvc.perform(patch("/flights/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithMockUser(username = "owner3", authorities = "USER")
+    public void shouldReturnUpdatedFlightOnFlightPatchRequestWhenUserIsResourceOwner() throws Exception {
+        Flight initial = flightService.findById(3L).orElseThrow();
+        patchByIdAndExpect(3L, updatedFlight2Json, status().isOk());
+
+        Flight updated = flightService.findById(3L).orElseThrow();
+        assertThat(updated, is(not(equalTo(initial))));
+    }
+
+    @Test
+    @WithMockUser(username = "owner", authorities = "USER")
+    public void shouldDenyFlightPatchingWhenUserIsNotResourceOwner() throws Exception {
+        patchByIdAndExpect(1L, updatedFlight1Json, status().isUnauthorized());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void shouldDenyFlightPatchingWhenUserIsNotAuthenticated() throws Exception {
+        patchByIdAndExpect(1L, updatedFlight1Json, status().isUnauthorized());
+    }
+}
