bae67ec1ccc36c4ef49c6e77cedf0a05d5763ad7
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-10-31 14:04:03 +0100

    User service: created user controller

---
+++ user-service/src/test/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users(email, password, name)
+VALUES ('e@gmail.com', '12345678', 'Alexander'),
+       ('e2@gmail.com', '35733535', 'Mark'),
+       ('e3@gmail.com', 'kjhfi83974', 'Robert'),
+       ('e4@gmail.com', '54kjrfr', 'George'),
+       ('e5@gmail.com', '444grgr', 'George'),
+       ('e6@gmail.com', '444grgr', 'George');
