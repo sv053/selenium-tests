d1e38fb142a672e3aff195fc8ed6d0a86f0e2de2
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-10-31 18:15:38 +0100

    Airline service: created destination class

---
+++ airline-service/src/test/java/air/airlineservice/service/flight/DestinationTest.java
@@ -0,0 +1,41 @@
+package air.airlineservice.service.flight;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Tag("category.UnitTest")
+public class DestinationTest {
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void shouldPassValidationWhenHasValidData() {
+        Destination destination = Destination.builder()
+                .withCountry("Country")
+                .withAirport("airport")
+                .withGate(3L)
+                .build();
+
+        int errors = validator.validate(destination).size();
+        assertThat(errors, is(0));
+    }
+
+    @Test
+    public void shouldNotPassValidationWhenHasInvalidData() {
+        Destination destination = new Destination();
+
+        int errors = validator.validate(destination).size();
+        assertThat(errors, is(3));
+    }
+}
