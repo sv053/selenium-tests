c9684d12f469ca98a92e6ec33b6018adba15711f
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-10-31 14:29:50 +0100

    Created local k8s run script

---
+++ deployment/minikube_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author : Alexander Tolpeko
+
+echo "Deleting existing minikube machine"
+minikube delete
+
+echo "Creating new minikube machine"
+minikube start
+minikube addons enable ingress
+
+echo "Spinning up kubernetes cluster"
+
+# Ingress
+kubectl create -f ingress-deployment.yaml
+
+# MySQL
+kubectl create -f mysql/mysql-storage.yaml
+kubectl create -f mysql/mysql-secret.yaml
+kubectl create -f mysql/mysql-deployment.yaml
+
+# User service
+kubectl create -f user-service/user-service-secret.yaml
+kubectl create -f user-service/user-service-deployment.yaml
+
+echo "Running tunnel. You can access the application on http://localhost/"
+minikube tunnel
