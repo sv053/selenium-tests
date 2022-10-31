f57d72bc445cdb1d757b111de8d5648275b92892
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-10-31 12:31:28 +0100

    User service: created user domain class

---
+++ user-service/src/test/java/solera/userservice/service/UserTest.java
@@ -0,0 +1,41 @@
+package solera.userservice.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Tag("category.UnitTest")
+public class UserTest {
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void shouldPassValidationWhenHasValidData() {
+        User user = User.builder()
+                .withEmail("email@gmail.com")
+                .withPassword("password")
+                .withName("name")
+                .build();
+
+        int errors = validator.validate(user).size();
+        assertThat(errors, is(0));
+    }
+
+    @Test
+    public void shouldNotPassValidationWhenHasInvalidData() {
+        User user = new User();
+
+        int errors = validator.validate(user).size();
+        assertThat(errors, is(3));
+    }
+}
