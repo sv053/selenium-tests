3e2b226104482d6fc0173e3256fba840faf243f0
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-01 12:25:17 +0100

    Airline service: flight access handler refactoring

--- airline-service/src/main/java/air/airlineservice/web/flights/FlightAccessHandler.java
+++ airline-service/src/main/java/air/airlineservice/web/flights/FlightAccessHandler.java
@@ -1,28 +1,20 @@
 package air.airlineservice.web.flights;
 
-import air.airlineservice.service.airline.Airline;
-import air.airlineservice.service.airline.AirlineService;
+import air.airlineservice.web.airline.AirlineAccessHandler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Collection;
-import java.util.Optional;
 
 @Component
 public class FlightAccessHandler {
-    private static final Logger logger = LogManager.getLogger(FlightAccessHandler.class);
-    private final AirlineService airlineService;
+    private final AirlineAccessHandler accessHandler;
 
     @Autowired
-    public FlightAccessHandler(AirlineService airlineService) {
-        this.airlineService = airlineService;
+    public FlightAccessHandler(AirlineAccessHandler accessHandler) {
+        this.accessHandler = accessHandler;
     }
 
     /**
@@ -34,34 +26,7 @@ public class FlightAccessHandler {
      * @return true if access is available, false otherwise
      */
     public boolean canPost(long airlineId) {
-        try {
-            Optional<Airline> airline = airlineService.findById(airlineId);
-            if (airline.isEmpty()) {
-                return false;
-            } else {
-                return hasAccess(airline.get().getOwner());
-            }
-        } catch (Exception e) {
-            logger.error(e);
-            return false;
-        }
-    }
-
-    private boolean hasAccess(String airlineOwner) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!authentication.isAuthenticated()) {
-            return false;
-        } else if (hasRole(authentication, "ADMIN")) {
-            return true;
-        } else {
-            return authentication.getName().equals(airlineOwner);
-        }
-    }
-
-    private boolean hasRole(Authentication authentication, String role) {
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        return authorities.stream()
-                .anyMatch(authority -> authority.getAuthority().equals(role));
+        return accessHandler.canPatch(airlineId);
     }
 
     /**
