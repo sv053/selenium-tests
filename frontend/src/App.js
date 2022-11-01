1cf5c82439808214ec7befba6c65207acdfc90f9
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-01 20:20:44 +0100

    Frontend: updated main function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
 import {HashRouter, Route, Routes, Navigate} from "react-router-dom"
+import {useState} from "react";
 import NotificationContainer from "react-notifications/lib/NotificationContainer"
 import NavigationBar from "./components/NavigationBar"
+import AccountPage from "./pages/AccountPage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ErrorPage from "./pages/ErrorPage";
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+
+    const [account, setAccount] = useState({
+        data: null,
+        loading: false
+    })
+
+    const loadAccount = () => {
+        console.log("Account opened")
+    }
+
+    const signInPressed = (email, password) => {
+        console.log("Sign in: " + email + " " + password)
+    }
+
+    const signUpPressed = (formData) => {
+        console.log("Sign up: " +
+            formData.email + " " +
+            formData.password + " " +
+            formData.repeatPassword + " " +
+            formData.name + " " +
+            formData.nationality + " " +
+            formData.passportNumber + " " +
+            formData.age)
+    }
+
     return (
         <HashRouter>
-          <NotificationContainer/>
-          <div className="layout">
-            <NavigationBar/>
-            <Routes>
-              <Route exact path="/" element={<Navigate to="/flights"/>}/>
-              <Route exact path="/flights" element={}/>
-              <Route exact path="/account" element={}/>
-              <Route exact path="/account/sign-in" element={}/>
-              <Route exact path="/account/sign-up" element={}/>
-              <Route exact path="/error" element={}/>
-              <Route exact path="*" element={}/>
-            </Routes>
-          </div>
+            <NotificationContainer/>
+            <div className="layout">
+                <NavigationBar/>
+                <Routes>
+                    <Route exact path="/" element={<Navigate to="/accounts"/>}/>
+                    <Route exact path="/account" element={
+                        <AccountPage data={account.data}
+                                     loading={account.loading}
+                                     onLoad={loadAccount}/>}/>
+                    <Route exact path="/account/sign-in" element={
+                        <SignInPage onSubmit={signInPressed}/>}/>
+                    <Route exact path="/account/sign-up" element={
+                        <SignUpPage onSubmit={signUpPressed}/>}/>
+                    <Route exact path="/error" element={<ErrorPage/>}/>
+                    <Route exact path="*" element={<ErrorPage message="Not found"/>}/>
+                </Routes>
+            </div>
         </HashRouter>
     );
 }
