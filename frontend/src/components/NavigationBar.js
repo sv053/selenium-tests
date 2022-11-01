465c4d77ce4744802c4b548ff993912e359dc176
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-01 19:46:23 +0100

    Frontend: created navigation bar

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from 'react-router-dom'
+
+const NavigationBar = () => {
+    return (
+        <Navbar bg="dark" variant="dark" id="nav-bar">
+            <Container>
+                <Link to="/" className="navbar-brand" id="main-link">AirTickets</Link>
+                <Nav className="me-auto">
+                    <Link to="/cart" className="nav-link" id="cart-link">Account</Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavigationBar
