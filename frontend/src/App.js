e30cfff9e30d04934f9a42a3c8e0bde074bf1318
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-01 21:59:16 +0100

    Frontend: created basic account methods in main function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,34 +7,37 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer/Footer";
+import {getUserByEmail, postUser} from "./api/UserApi";
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-
     const [account, setAccount] = useState({
         data: null,
         loading: false
     })
 
     const loadAccount = () => {
-        console.log("Account opened")
+        if (account.data) {
+            setAccount({data: account.data, loading: false})
+        }
     }
 
     const signInPressed = (email, password) => {
-        console.log("Sign in: " + email + " " + password)
+        setAccount({data: null, loading: true})
+        getUserByEmail(email, password)
+            .then(data => setAccount({data: data, loading: false}))
+            .then(e => window.location = "#/account")
+            .catch(e => window.location = "#/error?message=" + e.message)
     }
 
     const signUpPressed = (formData) => {
-        console.log("Sign up: " +
-            formData.email + " " +
-            formData.password + " " +
-            formData.repeatPassword + " " +
-            formData.name + " " +
-            formData.nationality + " " +
-            formData.passportNumber + " " +
-            formData.age)
+        setAccount({data: null, loading: true})
+        postUser(formData)
+            .then(data => setAccount({data: data, loading: false}))
+            .then(e => window.location = "#/account")
+            .catch(e => window.location = "#/error?message=" + e.message)
     }
 
     return (
