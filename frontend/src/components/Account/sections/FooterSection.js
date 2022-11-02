d874ad8ab7566f98d96c60ce3ded33139d69829f
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-02 15:17:07 +0100

    Frontend: removed unused button from account

--- frontend/src/components/Account/sections/FooterSection.js
+++ frontend/src/components/Account/sections/FooterSection.js
@@ -4,9 +4,6 @@ import '../Account.css'
 const FooterSection = props => {
     return (
         <div className="account-form-content">
-            <div >
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </div>
             <p className="forgot-password text-right mt-2">
                 <button className="btn btn-primary button"
                         onClick={props.onSignOut}>
