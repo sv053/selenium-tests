3b268fabce45a3d69f08dcb5a08374dda1919202
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-02 10:24:10 +0100

    Frontend: created flight card component

---
+++ frontend/src/components/Flight/FlightCard/FlightCard.js
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types"
+import {Button} from "@mui/material"
+
+import './FlightCard.css'
+
+const FlightCard = props => {
+    const date = toPrettyDate(props.dateTime)
+    const image = "data:image/png;base64, " + props.image.replace(/(\r\n|\n|\r)/gm, "")
+    return (
+        <div className="card-form">
+            <div className="card-form-content">
+                <div id="flight" className="flight">
+                    <div className="part-1"
+                         style={{ background: "url(\"" +  image + "\")  no-repeat center" }}>
+                    </div>
+                    <div className="part-2">
+                        <h4 className="title">{props.origin + " -> " + props.destination}</h4>
+                        <h4>{props.airline + ", " + date}</h4>
+                    </div>
+                    <div className="btn-group group">
+                        <Button variant="outline-info info button" onClick={props.actionLeft}>
+                            {props.buttonLeftText}
+                        </Button>
+                        <Button variant="primary info button" onClick={props.actionRight}>
+                            {props.buttonRightText}
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+FlightCard.propTypes = {
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    origin: PropTypes.string.isRequired,
+    destination: PropTypes.string.isRequired,
+    airline: PropTypes.string.isRequired,
+    dateTime: PropTypes.string.isRequired,
+    buttonLeftText: PropTypes.string.isRequired,
+    buttonRightText: PropTypes.string.isRequired,
+    actionRight: PropTypes.func.isRequired,
+    actionLeft: PropTypes.func.isRequired
+};
+
+const toPrettyDate = date => {
+    const toMonthName = monthNumber => {
+        const date = new Date();
+        date.setMonth(monthNumber - 1);
+        return date.toLocaleString([], { month: 'long',});
+    }
+
+    const year = date.slice(0, 4)
+    const month = toMonthName(date.slice(5, 7))
+    const day = date.slice(8, 10)
+    const time = date.slice(11, 16)
+
+    return day + " " + month + " " + year + ", " + time
+}
+
+
+export default FlightCard
