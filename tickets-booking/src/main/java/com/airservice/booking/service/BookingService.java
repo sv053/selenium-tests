3d7256ea0be1719ce2685997d3f1017e176602f5
Tolpeko Alexander <alexander.tolpeko@solera.com>
2022-11-04 09:33:18 +0100

    Booking service: implemented kafka messaging

--- tickets-booking/src/main/java/com/airservice/booking/service/BookingService.java
+++ tickets-booking/src/main/java/com/airservice/booking/service/BookingService.java
@@ -3,6 +3,12 @@ package com.airservice.booking.service;
 import com.airservice.booking.exception.EntityNotFoundException;
 import com.airservice.booking.model.Booking;
 import com.airservice.booking.repository.BookingRepository;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,15 +16,20 @@ import java.util.Optional;
 
 @Service
 public class BookingService {
+   private final Logger logger = LogManager.getLogger(BookingService.class);
 
    private final BookingRepository bookingRepository;
+   private final KafkaTemplate<String, String> kafkaTemplate;
 
-   public BookingService(BookingRepository bookingRepository) {
+   @Autowired
+   public BookingService(KafkaTemplate<String, String> kafkaTemplate,
+                         BookingRepository bookingRepository) {
       this.bookingRepository = bookingRepository;
+      this.kafkaTemplate = kafkaTemplate;
    }
 
    public List<Booking> findAllBookings() {
-      return (List<Booking>) bookingRepository.findAll();
+      return bookingRepository.findAll();
    }
 
    public void removeBooking(Long bookingId) {
@@ -38,7 +49,17 @@ public class BookingService {
    }
 
    public Booking createBooking(Booking booking) {
-      return bookingRepository.saveAndFlush(booking);
+      Booking saved = bookingRepository.saveAndFlush(booking);
+      sendMessage("Booking made: " + saved.getId());
+      return saved;
+   }
+
+   private void sendMessage(String message) {
+      try {
+         kafkaTemplate.send("order_statistics", message);
+      } catch (Exception e) {
+         logger.error("Kafka error:" + e);
+      }
    }
 }
 
