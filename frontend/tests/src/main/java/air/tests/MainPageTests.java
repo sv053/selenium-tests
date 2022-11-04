dadab33dbd66c51777e7223fcfdb3e01b6631f02
Svetlana.Shemiakina <scenacheros@yandex.ru>
2022-11-04 15:24:07 +0100

    main page class renamed

--- frontend/tests/src/main/java/air/tests/MainPageTests.java
+++ frontend/tests/src/main/java/air/tests/MainPageTests.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FrontendTests {
+public class MainPageTests {
     private static WebDriver driver;
 
     @BeforeAll
